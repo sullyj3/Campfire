3e45715dcb49495f0814ff9c4fbd7d1f54b825af
James Sully <sullyj3@gmail.com>
2019-07-14 01:40:50 +1000

    refactor Routes a little

--- backend/src/Routes.hs
+++ backend/src/Routes.hs
@@ -12,20 +12,27 @@ import Network.HTTP.Types (badRequest400, notFound404)
 
 import Story (_meta, ErrMsg(..), allExampleStories, lookupStoryByID)
 
+---------------------------
+
 routes = do
-  get "/stories" $ do
-    json $ _meta <$> allExampleStories
-  get "/story/:id" $ do
-    reqID <- param "id" :: ActionM Text
-    case decimal reqID of
-      Right (reqIDint, "") -> case lookupStoryByID reqIDint allExampleStories of
-        Just stry -> json stry
-        Nothing -> do
-          status notFound404
-          json $ ErrMsg $ "ID '" <> reqID <> "' not found!"
-      Right (reqIDint, _) -> do
-        status badRequest400
-        json $ ErrMsg $ "bad request: " <> reqID
-      Left _ -> do
-        status badRequest400
-        json $ ErrMsg $ "bad request: " <> reqID
+  get "/stories"   stories
+  get "/story/:id" story
+
+---------------------------
+
+stories = json $ _meta <$> allExampleStories
+
+story = do
+  reqID <- param "id" :: ActionM Text
+  case decimal reqID of
+    Right (reqIDint, "") -> case lookupStoryByID reqIDint allExampleStories of
+      Just stry -> json stry
+      Nothing -> do
+        status notFound404
+        json $ ErrMsg $ "ID '" <> reqID <> "' not found!"
+    Right (reqIDint, _) -> badRequest reqID
+    Left _              -> badRequest reqID
+
+badRequest req = do
+  status badRequest400
+  json $ ErrMsg $ "bad request: " <> req
