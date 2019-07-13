e7ba8fb2b49738f0a2aec1fcc12c6ce62cb4a023
James Sully <sullyj3@gmail.com>
2019-07-14 01:49:34 +1000

    Simplify routing

--- backend/src/Routes.hs
+++ backend/src/Routes.hs
@@ -23,16 +23,7 @@ routes = do
 stories = json $ _meta <$> allExampleStories
 
 story = do
-  reqID <- param "id" :: ActionM Text
-  case decimal reqID of
-    Right (reqIDint, "") -> case lookupStoryByID reqIDint allExampleStories of
-      Just stry -> json stry
-      Nothing -> do
-        status notFound404
-        json $ ErrMsg $ "ID '" <> reqID <> "' not found!"
-    Right (reqIDint, _) -> badRequest reqID
-    Left _              -> badRequest reqID
-
-badRequest req = do
-  status badRequest400
-  json $ ErrMsg $ "bad request: " <> req
+  reqID <- param "id" :: ActionM Int
+  case lookupStoryByID reqID allExampleStories of
+    Just stry -> json stry
+    Nothing   -> status notFound404
